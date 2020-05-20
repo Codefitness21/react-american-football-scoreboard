f2ae991f7c729f90a61c57a0526c039ad132468f
Kelly <kelly.m.hill2115@gmail.com>
2020-05-19 19:53:44 -0500

    Updates

---
+++ precache-manifest.0e79289677c15f3ea28aaa1cc50bbc9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69bd3febcd988b71c13d03f68a5038ec",
+    "url": "/react-american-football-scoreboard/index.html"
+  },
+  {
+    "revision": "a09189f7054a75013e8f",
+    "url": "/react-american-football-scoreboard/static/css/main.e6dc28a0.chunk.css"
+  },
+  {
+    "revision": "c272f52c06fda07103b0",
+    "url": "/react-american-football-scoreboard/static/js/2.18c8bb73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-american-football-scoreboard/static/js/2.18c8bb73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a09189f7054a75013e8f",
+    "url": "/react-american-football-scoreboard/static/js/main.01ce11f4.chunk.js"
+  },
+  {
+    "revision": "dcd7af3b93f8ef0eb243",
+    "url": "/react-american-football-scoreboard/static/js/runtime-main.2b5090e3.js"
+  },
+  {
+    "revision": "1abd8c1a58598974d8415573764ba76a",
+    "url": "/react-american-football-scoreboard/static/media/Ozone.1abd8c1a.ttf"
+  }
+]);
