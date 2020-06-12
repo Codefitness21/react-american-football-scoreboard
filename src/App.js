cecc1c5d881c7cba2f608ad32395c1db779552ac
Kelly <kelly.m.hill2115@gmail.com>
2020-06-12 16:00:38 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ function App() {
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Falcons</h2>
+            <h2 className="home__name">Saints</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
             <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Patriots</h2>
+            <h2 className="away__name">Falcons</h2>
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
